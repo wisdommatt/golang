025863f6e1f5a0541bb95ae850c38632c845e9a7
Wizdom Matthew <talk2wisdommatt@gmail.com>
2020-10-29 23:13:27 +0100

    Moved the whole programm to server.go

--- server.go
+++ server.go
@@ -1,16 +1,98 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
-
-	"github.com/wisdommatt/internweb/routers"
+	"strconv"
 )
 
+type user struct {
+	ID          int    `json:"id"`
+	FullName    string `json:"name"`
+	Email       string `json:"email"`
+	UserName    string `json:"username"`
+	Password    string `json:"password"`
+	PhoneNumber string `json:"phonenumber"`
+}
+
+var users = []user{}
+
+func (usr *user) save() {
+	usr.ID = len(users) + 1
+	users = append(users, *usr)
+}
+
 func main() {
-	http.HandleFunc("/users/new", routers.CreateUserHandler)
-	http.HandleFunc("/users/all", routers.ReadUsersHandler)
-	http.HandleFunc("/users/single", routers.ReadUserHandler)
-	http.HandleFunc("/users/single/update", routers.UpdateUserHandler)
+	http.HandleFunc("/users/new", CreateUserHandler)
+	http.HandleFunc("/users/all", ReadUsersHandler)
+	http.HandleFunc("/users/single", ReadUserHandler)
+	http.HandleFunc("/users/single/update", UpdateUserHandler)
 
 	http.ListenAndServe(":8000", nil)
+}
+
+func CreateUserHandler(response http.ResponseWriter, request *http.Request) {
+	var submittedUser user
+
+	err := json.NewDecoder(request.Body).Decode(&submittedUser)
+	if err != nil {
+		response.WriteHeader(http.StatusBadRequest)
+		response.Write([]byte("Invalid data submitted !"))
+		return
+	}
+
+	submittedUser.save()
+	json.NewEncoder(response).Encode(submittedUser)
+}
+
+func ReadUsersHandler(response http.ResponseWriter, request *http.Request) {
+	json.NewEncoder(response).Encode(users)
+}
+
+func ReadUserHandler(response http.ResponseWriter, request *http.Request) {
+	id := request.URL.Query().Get("userid")
+	userID, err := strconv.Atoi(id)
+
+	if err != nil {
+		response.WriteHeader(http.StatusBadRequest)
+		response.Write([]byte("Invalid user ID !"))
+		return
+	}
+
+	if userID > len(users) || userID < 1 {
+		response.WriteHeader(http.StatusBadRequest)
+		response.Write([]byte("User id is out of range"))
+		return
+	}
+
+	userWithID := users[userID-1]
+	json.NewEncoder(response).Encode(userWithID)
+}
+
+func UpdateUserHandler(response http.ResponseWriter, request *http.Request) {
+	id := request.URL.Query().Get("userid")
+	userID, err := strconv.Atoi(id)
+
+	if err != nil {
+		response.WriteHeader(http.StatusBadRequest)
+		response.Write([]byte("Invalid user ID !"))
+		return
+	}
+
+	if userID > len(users) || userID < 1 {
+		response.WriteHeader(http.StatusBadRequest)
+		response.Write([]byte("User id is out of range"))
+		return
+	}
+
+	userWithID := &users[userID-1]
+	err = json.NewDecoder(request.Body).Decode(userWithID)
+
+	if err != nil {
+		response.WriteHeader(http.StatusBadRequest)
+		response.Write([]byte("Invalid data submited"))
+		return
+	}
+
+	json.NewEncoder(response).Encode(userWithID)
 }
